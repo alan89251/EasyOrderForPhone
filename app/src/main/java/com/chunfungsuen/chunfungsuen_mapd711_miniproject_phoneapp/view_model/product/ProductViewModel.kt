019f2bb8012cf346a8e5170d4e4cd81080c3ff32
Alan <csuen2@my.centennialcollege.ca>
2022-12-09 08:12:44 -0400

    Add Product view model classes

---
+++ app/src/main/java/com/chunfungsuen/chunfungsuen_mapd711_miniproject_phoneapp/view_model/product/ProductViewModel.kt
@@ -0,0 +1,38 @@
+package com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.product
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.product.ProductModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.product.ProductRepository
+
+class ProductViewModel(
+    // dependency injection of productRepository
+    private val productRepository: ProductRepository
+)
+    : ViewModel()
+{
+    // used to filter the items shown on the product list
+    var brand: MutableLiveData<String> = MutableLiveData("")
+
+    var productList: LiveData<List<ProductModel>>? = null
+        private set
+
+    // init the live data of the product list
+    // when brand is empty, load all the product.
+    // otherwise, load the products of that brand only
+    fun initProductList() {
+        productList = Transformations.switchMap(brand, {
+            // return all products when brand name is empty
+            if (it == "") {
+                return@switchMap productRepository.getAllProduct()
+            }
+            return@switchMap productRepository.getProductByPhoneMake(it)
+        })
+    }
+
+    fun getProductById(productId: Int): LiveData<ProductModel>? {
+        return productRepository.getProductById(productId)
+    }
+}
