73a2eb701ba742de8b8be891e46c4f0eec28ecac
Alan <csuen2@my.centennialcollege.ca>
2022-12-09 07:29:57 -0400

    Add main activity layout and logic

--- app/src/main/java/com/chunfungsuen/chunfungsuen_mapd711_miniproject_phoneapp/MainActivity.kt
+++ app/src/main/java/com/chunfungsuen/chunfungsuen_mapd711_miniproject_phoneapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Navigate to the login activity
+     */
+    fun navigateToLogin(v: View) {
+        var intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
