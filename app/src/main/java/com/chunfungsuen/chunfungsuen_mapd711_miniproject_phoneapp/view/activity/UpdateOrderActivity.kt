c61e68775fbd88128b86ebf1967da0bf86692441
Alan <csuen2@my.centennialcollege.ca>
2022-12-09 08:41:36 -0400

    Add Update order activity and order list classes

---
+++ app/src/main/java/com/chunfungsuen/chunfungsuen_mapd711_miniproject_phoneapp/view/activity/UpdateOrderActivity.kt
@@ -0,0 +1,142 @@
+package com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view.activity
+
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import androidx.lifecycle.ViewModelProvider
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.R
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.customer.CustomerModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.customer.CustomerRepository
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.order.OrderModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.order.OrderRepository
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.product.ProductModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.product.ProductRepository
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.database.PhoneOrderServiceDatabase
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view.view_adapter.OrderListViewAdapter
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.customer.CustomerViewModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.customer.CustomerViewModelFactory
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.order.OrderViewModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.order.OrderViewModelFactory
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.product.ProductViewModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view_model.product.ProductViewModelFactory
+
+class UpdateOrderActivity : AppCompatActivity() {
+    private lateinit var customerViewModel: CustomerViewModel
+    private lateinit var orderViewModel: OrderViewModel
+    private lateinit var productViewModel: ProductViewModel
+    private lateinit var orderListView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_order)
+
+        // read from share preferences
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("phoneOrderServicePreferences", MODE_PRIVATE)
+        val userName = sharedPreferences.getString("userName", "").toString()
+
+        // config UIs
+        orderListView = findViewById(R.id.order_list)
+
+        // init database
+        val phoneOrderServiceDatabase = PhoneOrderServiceDatabase.getDatabaseClient(this@UpdateOrderActivity)
+
+        // create view model for customer
+        customerViewModel = ViewModelProvider(this,
+            CustomerViewModelFactory(
+                CustomerRepository(
+                    phoneOrderServiceDatabase!!.customerDao()
+                )
+            )
+        ).get(CustomerViewModel::class.java)
+
+        // create view model for product
+        productViewModel = ViewModelProvider(this,
+            ProductViewModelFactory(
+                ProductRepository(
+                    phoneOrderServiceDatabase!!.productDao()
+                )
+            )
+        ).get(ProductViewModel::class.java)
+
+        // create view model for order
+        orderViewModel = ViewModelProvider(this,
+            OrderViewModelFactory(
+                OrderRepository(
+                    phoneOrderServiceDatabase!!.orderDao()
+                )
+            )
+        ).get(OrderViewModel::class.java)
+
+        // init order list in the view model
+        orderViewModel.initOrderList()
+        orderViewModel.orderList!!.observe(this, ::updateOrderList)
+        // load customer info from the repository
+        customerViewModel.loadCustomer(userName)
+        customerViewModel.customer!!.observe(this, ::loadOrderListForCustomer)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.phone_order_service_menu, menu)
+        return true
+    }
+
+    /**
+     * Navigate to the activity of the selected menu item
+     * Do nothing if the user select the current activity
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_phone_model_list -> {
+                val intent = Intent(this@UpdateOrderActivity, OrderActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.menu_update_customer_info -> {
+                val intent = Intent(this@UpdateOrderActivity, UpdateCustomerActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return true
+    }
+
+    // Load order list from the repository for the logined user
+    // This function is used as Observer for the customer view model
+    private fun loadOrderListForCustomer(customer: CustomerModel) {
+        // set orderViewModel.customerId will trigger the reload of the orderList
+        orderViewModel.customerId!!.value = customer.CustId
+    }
+
+    private fun updateOrderList(orderList: List<OrderModel>) {
+        if (orderList.isEmpty()) {
+            return
+        }
+
+        orderListView.adapter = OrderListViewAdapter(
+            customerViewModel.customer!!.value!!,
+            ::getProductByIdAsync,
+            ::updateOrder,
+            this,
+            android.R.layout.simple_list_item_1,
+            orderList
+        )
+    }
+
+    /**
+     * Get product info from repository async-ly
+     * After received the result, call the callback: "onResult"
+     */
+    private fun getProductByIdAsync(productId: Int, onResult: (ProductModel) -> Unit) {
+        productViewModel.getProductById(productId)!!.observe(this, onResult)
+    }
+
+    /**
+     * Update the order that saved in the repository
+     */
+    private fun updateOrder(order: OrderModel) {
+        orderViewModel.updateOrder(order)
+    }
+}
---
+++ app/src/main/java/com/chunfungsuen/chunfungsuen_mapd711_miniproject_phoneapp/view/view_adapter/OrderListViewAdapter.kt
@@ -0,0 +1,88 @@
+package com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.view.view_adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.R
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.customer.CustomerModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.order.OrderModel
+import com.chunfungsuen.chunfungsuen_mapd711_miniproject_phoneapp.data_model.product.ProductModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatterBuilder
+
+class OrderListViewAdapter (
+    private val customer: CustomerModel,
+    private val getProductByIdAsync: (Int, (ProductModel) -> Unit) -> Unit,
+    private val updateOrder: (OrderModel) -> Unit,
+    context: Context,
+    resource: Int,
+    objects: List<OrderModel>)
+    : ArrayAdapter<OrderModel>(context, resource, objects)
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // inflate item view
+        val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView: View = inflater.inflate(R.layout.order_list_item_layout, parent, false)
+
+        val order = getItem(position)!!
+        // show order info in UI
+        itemView.findViewById<TextView>(R.id.order_list_item_order_id).text = order.OrderId.toString()
+        itemView.findViewById<TextView>(R.id.order_list_item_customer_id).text = order.CustId.toString()
+        itemView.findViewById<TextView>(R.id.order_list_item_product_id).text = order.ProductId.toString()
+        itemView.findViewById<TextView>(R.id.order_list_item_order_date).text = order.OrderDate
+        itemView.findViewById<TextView>(R.id.order_list_item_status).text = order.Status
+        // config cancel order button
+        val cancelOrderBtn = itemView.findViewById<ImageButton>(R.id.cancel_order_btn)
+        cancelOrderBtn.setOnClickListener {
+            onCancelButtonClicked(order, itemView)
+        }
+        // disable and hide the cancel order button if the order is already cancelled
+        if (order.Status == context.resources.getString(R.string.order_status_text_cancelled)) {
+            cancelOrderBtn.isEnabled = false
+            cancelOrderBtn.isVisible = false
+        }
+
+        // get product info from repository and then show them in the UI
+        getProductByIdAsync(order.ProductId, {
+            itemView.findViewById<TextView>(R.id.order_list_item_phone_model).text = it.PhoneModel
+            itemView.findViewById<TextView>(R.id.order_list_item_phone_make).text = it.PhoneMake
+            itemView.findViewById<TextView>(R.id.order_list_item_phone_color).text = it.PhoneColor
+            itemView.findViewById<TextView>(R.id.order_list_item_storage_capacity).text = it.StorageCapacity
+            itemView.findViewById<TextView>(R.id.order_list_item_price).text = "$" + it.Price.toString()
+        })
+
+        return itemView
+    }
+
+    private fun onCancelButtonClicked(order: OrderModel, itemView: View) {
+        val currentDate = LocalDateTime.now() // get current date
+        // parse the order date
+        val orderDate = LocalDateTime.from(
+            DateTimeFormatterBuilder()
+                .appendPattern("dd/MM/yyyy HH:mm:ss")
+                .toFormatter()
+                .parse(
+                    itemView.findViewById<TextView>(R.id.order_list_item_order_date).text.toString() + " 00:00:00"
+                )
+        )
+        // Only allow to cancel at least 24 hours before the order date
+        if (currentDate.isAfter(
+                orderDate.minusHours(24)
+            ))
+        {
+            Toast.makeText(context, "Only allow to cancel at least 24 hours before the order date", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        // Cancel the order which change its status to "Cancelled"
+        order.Status = context.resources.getString(R.string.order_status_text_cancelled)
+        updateOrder(order)
+    }
+}
